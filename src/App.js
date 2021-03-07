9e7e2bc9ea273129b669797823e1ad8f5443f6df
danielllk <danielk2091@gmail.com>
2021-03-07 21:16:14 +0000

    in process

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.scss";
+import checkBtn from "./img/check.svg";
+import deleteBtn from "./img/delete.svg";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [taskArray, setTaskArray] = useState([]);
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    setTaskArray((taskArray) => [...taskArray, task]);
+    //localStorage.setItem("tasks", taskArray);
+    console.log(task);
+    console.log(taskArray);
+    setTask("");
+    e.preventDefault();
+    //console.log(localStorage);
+  };
+
+  useEffect(() => {}, []);
+
+  const deletePost = (task) => {
+    const newTasks = [...taskArray];
+    newTasks.splice(taskArray.indexOf(task), 1);
+    setTaskArray(newTasks);
+    //console.log(taskArray.indexOf(task));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add the task"
+            value={task}
+            onChange={handleChange}
+          />
+          <button value="Submit" type="submit">
+            submit
+          </button>
+        </form>
+        <div>
+          <ul>
+            {taskArray.map((task, index) => (
+              <li key={index}>
+                <div>
+                  <span>
+                    <img src={checkBtn} alt="check-btn" />
+                  </span>
+                  <span onClick={() => deletePost(task)}>
+                    <img src={deleteBtn} alt="delete-btn" />
+                  </span>
+                </div>
+                {task}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
